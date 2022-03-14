7ab28dd14d366092db4620b030589564d245752d
pMichaell <michal.pawlowicz112@gmail.com>
2022-03-14 12:13:11 +0100

    mobile animation for planets created

--- src/pages/DestinationPage/DestinationPage.tsx
+++ src/pages/DestinationPage/DestinationPage.tsx
@@ -2,66 +2,55 @@ import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { Destination } from "../../interfaces";
 import classes from "./DestinationPage.module.css";
-import { useSwipeable } from "react-swipeable";
 import { wrap } from "popmotion";
+import { AnimatePresence, motion } from "framer-motion";
 import Planet from "./Planet";
-import { AnimatePresence } from "framer-motion";
 
 const data = require("../../data.json");
 const destinations: Destination[] = data.destinations;
 
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+
 const DestinationPage = () => {
   const [planetInfo, setPlanetInfo] = useState<Destination>();
-  const [currentIndex, setCurrentIndex] = useState<number>();
-
-  const getPlanetInfo = (planetName: string) => {
-    return destinations.filter((planet) => planet.name === planetName).shift();
-  };
-
-  const getCurrentIndex = (planetName: string) => {
-    setCurrentIndex(
-      destinations.findIndex((planet) => planet.name === planetName)
-    );
-  };
-
   const [[page, direction], setPage] = useState([0, 0]);
 
-  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
-  // then wrap that within 0-2 to find our image ID in the array below. By passing an
-  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
-  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
-  const imageIndex = wrap(0, destinations.length, page);
+  const planetIndex = wrap(0, destinations.length, page);
 
   const paginate = (newDirection: number) => {
     setPage([page + newDirection, newDirection]);
   };
 
   useEffect(() => {
-    const currentPlanet = localStorage.getItem("planet") ?? "Moon";
-    console.log(currentPlanet);
-    setPlanetInfo(getPlanetInfo(currentPlanet));
-    getCurrentIndex(currentPlanet);
-  }, [currentIndex]);
-
-  const switchDestination = (right: boolean) => {
-    let newIndex: number;
-    if (right) {
-      newIndex = currentIndex === 3 ? 0 : currentIndex! + 1;
-    } else {
-      newIndex = currentIndex === 0 ? 3 : currentIndex! - 1;
-    }
-    setCurrentIndex(newIndex);
-    localStorage.setItem("planet", destinations[newIndex].name);
-  };
-
-  const handlers = useSwipeable({
-    onSwipedRight: () => switchDestination(false),
-    onSwipedLeft: () => switchDestination(true),
-  });
+    setPlanetInfo(destinations[planetIndex]);
+  }, [planetIndex]);
 
   return (
     <div
-      {...handlers}
       className={clsx(
         "fulfillParent",
         "flex",
@@ -98,10 +87,43 @@ const DestinationPage = () => {
       </ul>
       <AnimatePresence exitBeforeEnter>
         {destinations
-          .filter((_, index) => index === currentIndex)
-          .map((dest) => (
-            <Planet key={currentIndex} {...dest} />
-          ))}
+          .filter((_, index) => index === planetIndex)
+          .map(() => {
+            return (
+              <motion.div
+                className={clsx("flex", classes.planetDiv)}
+                key={page}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: {
+                    type: "tween",
+                    stiffness: 300,
+                    damping: 30,
+                    duration: 0.2,
+                  },
+                  opacity: { duration: 0.2 },
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = swipePower(offset.x, velocity.x);
+
+                  if (swipe < -swipeConfidenceThreshold) {
+                    paginate(1);
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    paginate(-1);
+                  }
+                }}
+              >
+                <Planet {...destinations[planetIndex]} />
+              </motion.div>
+            );
+          })}
       </AnimatePresence>
     </div>
   );
--- src/pages/DestinationPage/Planet.tsx
+++ src/pages/DestinationPage/Planet.tsx
@@ -1,26 +1,15 @@
-import { motion } from "framer-motion";
 import classes from "./DestinationPage.module.css";
 import clsx from "clsx";
 import { Destination } from "../../interfaces";
 
-const Planet = (planetInfo: Destination) => {
+const Planet = ({ name, description, distance, travel }: Destination) => {
   return (
-    <motion.div
-      initial={{ x: 300, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: -300, opacity: 0 }}
-      transition={{ type: "tween", duration: 0.1 }}
-      className={clsx("flex", classes.planetDiv)}
-    >
+    <>
       <div>
-        <img alt="planet" className={classes[planetInfo?.name!]} />
+        <img alt="planet" className={classes[name!]} />
       </div>
-      <h2 className={clsx("uppercase", "ffSerif", "fs800")}>
-        {planetInfo?.name}
-      </h2>
-      <p className={clsx("textAccent", classes.description)}>
-        {planetInfo?.description}
-      </p>
+      <h2 className={clsx("uppercase", "ffSerif", "fs800")}>{name}</h2>
+      <p className={clsx("textAccent", classes.description)}>{description}</p>
       <div className={classes.break} />
       <div className={clsx("flow", "flex", classes.additionalInfo)}>
         <p
@@ -34,9 +23,7 @@ const Planet = (planetInfo: Destination) => {
         >
           Avg. Distance
         </p>
-        <p className={clsx("uppercase", "fs500", "ffSerif")}>
-          {planetInfo?.distance}
-        </p>
+        <p className={clsx("uppercase", "fs500", "ffSerif")}>{distance}</p>
         <p
           className={clsx(
             "uppercase",
@@ -48,11 +35,9 @@ const Planet = (planetInfo: Destination) => {
         >
           Est. Travel Time
         </p>
-        <p className={clsx("uppercase", "fs500", "ffSerif")}>
-          {planetInfo?.travel}
-        </p>
+        <p className={clsx("uppercase", "fs500", "ffSerif")}>{travel}</p>
       </div>
-    </motion.div>
+    </>
   );
 };
 
